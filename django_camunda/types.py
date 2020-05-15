d7c3a136ababbfac3e5710c691f52c2408214e39
Sergei Maertens <sergei@maykinmedia.nl>
2020-05-15 17:01:56 +0200

    :sparkles: Provide some variable serialization utilities

--- django_camunda/types.py
+++ django_camunda/types.py
@@ -3,3 +3,6 @@ from typing import Dict, List, Union
 JSONPrimitive = Union[str, int, None, float]
 JSONValue = Union[JSONPrimitive, "JSONObject", List["JSONValue"]]
 JSONObject = Dict[str, JSONValue]
+
+ProcessVariable = Dict[str, JSONPrimitive]  # {"type": "Integer", "value": 42}
+ProcessVariables = Dict[str, ProcessVariable]
--- django_camunda/utils.py
+++ django_camunda/utils.py
@@ -1,6 +1,15 @@
-from typing import Dict, List, Union
+import json
+from datetime import date, datetime
+from typing import Any, Dict, List, Union
 
 import inflection
+from dateutil import parser
+
+from .types import ProcessVariable
+
+
+def noop(val):
+    return val
 
 
 def underscoreize(data: Union[List, Dict, str, None]) -> Union[List, Dict, str, None]:
@@ -19,3 +28,48 @@ def underscoreize(data: Union[List, Dict, str, None]) -> Union[List, Dict, str, 
         return new_data
 
     return data
+
+
+TYPE_MAP = {
+    bool: ("Boolean", noop),
+    date: (
+        "String",
+        lambda d: d.isoformat(),
+    ),  # Date object requires time information, which we don't have
+    datetime: ("Date", lambda d: d.isoformat()),
+    int: ("Integer", noop),
+    float: ("Double", noop),
+    str: ("String", noop),
+    type(None): ("Null", noop),
+    dict: ("Json", json.dumps),
+    list: ("Json", json.dumps),
+}
+
+
+REVERSE_TYPE_MAP = {
+    "date": parser.parse,
+    "json": json.loads,
+    "integer": int,
+    "short": int,
+    "long": int,
+}
+
+
+def serialize_variable(value: Any) -> ProcessVariable:
+    val_type = type(value)
+    if val_type not in TYPE_MAP:
+        raise NotImplementedError(f"Type {val_type} is not implemented yet")
+
+    type_name, converter = TYPE_MAP[val_type]
+    return {"type": type_name, "value": converter(value)}
+
+
+def deserialize_variable(variable: ProcessVariable) -> Any:
+    var_type = variable.get("type", "String")
+    converter = REVERSE_TYPE_MAP.get(var_type.lower())
+    if converter:
+        value = converter(variable["value"])
+    else:
+        value = variable["value"]  # a JSON primitive that maps to proper python objects
+
+    return value
