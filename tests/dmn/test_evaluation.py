56fcbaa2a727761f4331942232e29ab5746d0ed9
Sergei Maertens <sergei@maykinmedia.nl>
2022-06-14 15:20:14 +0200

    :white_check_mark: Add tests for DMN evaluation

--- tests/dmn/test_evaluation.py
+++ tests/dmn/test_evaluation.py
@@ -4,12 +4,31 @@ import pytest
 
 from django_camunda.dmn import evaluate_dmn
 
-CAMUNDA_USER = os.getenv("CAMUNDA_USER", "demo")
-CAMUNDA_PASSWORD = os.getenv("CAMUNDA_PASSWORD", "demo")
-
 
 def test_evaluate_by_key(camunda_client, deployed_decision_definition):
-    import bpdb
+    input_vars = {
+        "direction": "incoming",
+        "port": 443,
+    }
 
-    bpdb.set_trace()
-    pass
+    result = evaluate_dmn(
+        "firewall_rules", input_values=input_vars, client=camunda_client
+    )
+
+    assert result == {"policy": "allow", "reason": "HTTPS traffic"}
+
+
+def test_evaluate_by_id(camunda_client, deployed_decision_definition):
+    input_vars = {
+        "direction": "incoming",
+        "port": 443,
+    }
+
+    result = evaluate_dmn(
+        "firewall_rules",
+        dmn_id=deployed_decision_definition["id"],
+        input_values=input_vars,
+        client=camunda_client,
+    )
+
+    assert result == {"policy": "allow", "reason": "HTTPS traffic"}
